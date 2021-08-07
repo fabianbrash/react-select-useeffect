479b518bcef47df51721866228dc93c42bc7a522
Fabian Brash <fabianbrash@gmail.com>
2021-08-07 12:36:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createServer } from 'miragejs';
+
+
+let server = createServer();
+
+server.get("/api/v1/pgs", {
+  pgs: [
+    {
+      id: 1, name: "VSS-MGMT-99"
+    },
+    {
+      id: 2, name: "vDS-NESTED-10"
+    },
+    {
+      id: 3, name: "VSS-VMTraffic-700",
+    }
+  ]
+})
+
 
 function App() {
+
+  const [ports, setPorts] = useState([]);
+  const [port, setPort] = useState('VSS-MGMT-99');
+
+  useEffect(() => {
+    fetch('/api/v1/pgs')
+    .then(response => response.json())
+    .then(data => setPorts(data.pgs))
+  },[])
+
+  const handleSelect = (e) => {
+    console.log(e);
+    setPort(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="portgroups">Choose a portgroup:</label>
+      <select name="portgroups" onChange={handleSelect}>
+        <optgroup label="PortGroups">
+        {ports && ports.map(p => (
+          <option value={p.name} key={p.id} required>{p.name}</option>
+        ))}
+        </optgroup>
+      </select>
+      <h2>You selected PG: {port}</h2>
     </div>
   );
 }
